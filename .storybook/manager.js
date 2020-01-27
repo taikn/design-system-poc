6a1984f617efccdaed167820a6cb059194647648
Edik <taikn@outlook.com>
2020-01-27 17:53:12 +0100

    Remove storysource addon

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -2,7 +2,6 @@ import { addons } from '@storybook/addons';
 import '@storybook/addon-docs';
 import '@storybook/addon-actions/register';
 import '@storybook/addon-links/register';
-import '@storybook/addon-storysource/register';
 import '@storybook/addon-knobs/register';
 import defaultTheme from './defaultTheme';
 
